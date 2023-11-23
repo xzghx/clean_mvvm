a0715bbb2b216110210bae2ada8e846c92e4c711
ghafoori.z <ghafoory.zahra@gmail.com>
2023-11-23 09:59:16 +0330

    add project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Clean mvvm project
+
+Flutter project written with clean and mvvm architecture.
+
+ 
---
+++ android/app/src/main/kotlin/com/example/clean_mvvm_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.clean_mvvm_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
