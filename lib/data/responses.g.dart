411f4f88e430d912e12198f22488a47e6c679ca4
ghafoori.z <ghafoory.zahra@gmail.com>
2023-12-02 15:45:00 +0330

    Add response, model, viewmodel

---
+++ lib/data/responses.g.dart
@@ -0,0 +1,62 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'responses.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+BaseResponse _$BaseResponseFromJson(Map<String, dynamic> json) => BaseResponse()
+  ..status = json['status'] as int?
+  ..message = json['message'] as String?;
+
+Map<String, dynamic> _$BaseResponseToJson(BaseResponse instance) =>
+    <String, dynamic>{
+      'status': instance.status,
+      'message': instance.message,
+    };
+
+CustomerResponse _$CustomerResponseFromJson(Map<String, dynamic> json) =>
+    CustomerResponse()
+      ..id = json['id'] as int?
+      ..name = json['name'] as String?
+      ..numOfNotifications = json['numOfNotifications'] as int?;
+
+Map<String, dynamic> _$CustomerResponseToJson(CustomerResponse instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'numOfNotifications': instance.numOfNotifications,
+    };
+
+ContactsResponse _$ContactsResponseFromJson(Map<String, dynamic> json) =>
+    ContactsResponse()
+      ..email = json['email'] as String?
+      ..phone = json['phone'] as String?
+      ..link = json['link'] as String?;
+
+Map<String, dynamic> _$ContactsResponseToJson(ContactsResponse instance) =>
+    <String, dynamic>{
+      'email': instance.email,
+      'phone': instance.phone,
+      'link': instance.link,
+    };
+
+LoginResponse _$LoginResponseFromJson(Map<String, dynamic> json) =>
+    LoginResponse()
+      ..status = json['status'] as int?
+      ..message = json['message'] as String?
+      ..customer = json['customer'] == null
+          ? null
+          : CustomerResponse.fromJson(json['customer'] as Map<String, dynamic>)
+      ..contact = json['contact'] == null
+          ? null
+          : ContactsResponse.fromJson(json['contact'] as Map<String, dynamic>);
+
+Map<String, dynamic> _$LoginResponseToJson(LoginResponse instance) =>
+    <String, dynamic>{
+      'status': instance.status,
+      'message': instance.message,
+      'customer': instance.customer,
+      'contact': instance.contact,
+    };
